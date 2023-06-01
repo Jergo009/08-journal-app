89d9261c81b1869a2dd277b939223f5555f51d45
Jergo009 <88161320+Jergo009@users.noreply.github.com>
2023-06-01 10:24:46 -0600

    Boton Flotante
    agragamos boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
 
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 
 export const JournalPage = () => {
@@ -9,8 +11,25 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Nostrud ullamco occaecat proident amet irure velit nostrud aute magna. Ut amet aute Lorem pariatur commodo sunt pariatur quis commodo sunt do culpa nostrud eiusmod. Elit elit aliquip cupidatat esse. Sunt et pariatur enim et et excepteur enim incididunt id. Ea laboris consequat esse ut quis commodo nostrud ea dolore tempor proident id.</Typography> */}
       <NothingSelectedView />
-      {/* NothinSelected */}
-      {/* NoteView */}
+      
+      {/* <NoteView /> */}
+    <IconButton
+    size='large'
+    sx={{
+      color: 'white',
+      backgroundColor: 'error.main',
+      ':hover': {backgroundColor: 'error.main', opcicty: 0.9 },
+      position:'fixed',
+      right: 50,
+      bottom: 50
+
+    }}
+    
+    >
+      <AddOutlined sx={{fontSize: 30 }}/>
+    </IconButton>
+
+
     </JournalLayout>
   )
 }
